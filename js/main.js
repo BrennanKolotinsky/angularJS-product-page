c2c7bd62b2433bb6473535ebd2feb436a9856f48
Brennan Kolotinsky <BrennanKolotinsky@gmail.com>
2019-11-08 18:48:25 -0500

    created an angular module and controller, injected in a factory to create resting services and successfully pulling the product information

---
+++ js/main.js
@@ -0,0 +1,25 @@
+angular.module('sowingoModule', [])
+	.factory('RestServices', ['$http', function($http) {
+
+		var services = {};
+		services.getProducts = $http({
+			method: 'GET',
+			url: 'https://demo1064913.mockable.io/products'
+		});
+	  	
+		return services;
+	}])
+	.service('StateManager', function() {
+		this.words = 'awesome';
+	})
+	.controller('sowingoController', ['$scope', 'RestServices', 'StateManager', function($scope, RestServices, StateManager) {
+
+		$scope.words = StateManager.words;
+
+		RestServices.getProducts.then(function success(res) {
+			$scope.productData = res.data.products;
+			console.log(res.data.products);
+		});
+
+	}]);
+	
